b784847182f0937d05b7904be6088c4ac1299c4d
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-13 15:57:58 -0600

    Added SoundCloud link

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<title></title>
+		<title>About Skyler</title>
 	</head>
 
 	<body>
@@ -14,7 +14,7 @@
 		<p>
 			I've been coding in several different languages for three or four years. I've primarily used Game Maker: Studio and its aptly-named proprietary language Game Maker Language. I took two semesters of Java at UNM, and a semester of HTML, JavaScript, and SQL at CNM.
 			<br>
-			In my spare time I like to make music, art, and video games (and play them, too!). I have a SoundCloud where I post the latest songs I've been making.
+			In my spare time I like to make music, art, and video games (and play them, too!). I have a <a href="https://soundcloud.com/skylarity" target="_blank">SoundCloud</a> where I post the latest songs I've been making.
 			<br><br>
 			<span class="center">I can't wait to get to know everyone and start working on projects!</span>
 		</p>
