3bf9cd30ca9ea4dfb2f02d2c0a931f66f49ea16e
angelamaunz <alynnem0z@gmail.com>
2015-07-13 16:57:16 -0600

    update skyler's profile

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
 	<body>
 		<img src="images/robot.png" alt="Skylarity">
 		<h1>
-			Hi, I'm Skyler!
+			Hi, all, Skyler is too young to go to the pub and drink beer!
 		</h1>
 		<p>
 			I've been coding in several different languages for three or four years. I've primarily used Game Maker: Studio and its aptly-named proprietary language Game Maker Language. I took two semesters of Java at UNM, and a semester of HTML, JavaScript, and SQL at CNM.
 			<br>
+			<p>I wanta play one of his cool games.</p>
 			In my spare time I like to make music, art, and video games (and play them, too!). I have a <a href="https://soundcloud.com/skylarity" target="_blank">SoundCloud</a> where I post the latest songs I've been making.
 			<br><br>
 			<span class="center">I can't wait to get to know everyone and start working on projects!</span>
