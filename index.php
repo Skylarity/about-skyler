3b33c501d10eeff545100e201b88f4ad98414c32
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-13 15:32:08 -0600

    Added info and style.

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<title></title>
 	</head>
+
 	<body>
-		Hello, world!
+		<img src="images/robot-white.png" alt="Skylarity">
+		<h1>
+			Hi, I'm Skyler!
+		</h1>
+		<p>
+			I've been coding in several different languages for three or four years. I've primarily used Game Maker: Studio and its aptly-named proprietary language Game Maker Language. I took two semesters of Java at UNM, and a semester of HTML, JavaScript, and SQL at CNM.
+			<br>
+			In my spare time I like to make music, art, and video games (and play them, too!). I have a SoundCloud where I post the latest songs I've been making.
+			<br><br>
+			<span class="center">I can't wait to get to know everyone and start working on projects!</span>
+		</p>
 	</body>
 </html>
